a22c5cd266c0bf236b5d1b573e9f6458281d2305
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2024-08-01 16:37:21 -0700

    added comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,13 +18,19 @@ export default function Home() {
 }
 
 const FoldableMap = () => {
+  // Motion values for drag animation
   const xDrag = useMotionValue(0);
-  const [isFolded, setIsFolded] = useState(true);
+  const [isFolded, setIsFolded] = useState(true); // State to track if map is folded
+
+  // Transform functions for left and right sections
   const xLeftSection = useTransform(xDrag, [0, 300], ["100%", "0%"]);
   const xRightSection = useTransform(xDrag, [0, 300], ["-100%", "0%"]);
+
+  // Transform functions for center section
   const centerScale = useTransform(xDrag, [150, 300], [0, 1]);
   const centerBrightness = useTransform(xDrag, [150, 300], [0.2, 1]);
 
+  // Event listener for drag motion
   useMotionValueEvent(xDrag, "change", (currentX) => {
     if (currentX > 260) {
       setIsFolded(false);
@@ -36,12 +42,12 @@ const FoldableMap = () => {
   return (
     <div className="overflow-x-clip">
       <motion.div
-        animate={isFolded ? "folded" : "open"}
+        animate={isFolded ? "folded" : "open"} // Animation based on folded state
         variants={{
           open: { scale: 1 },
           folded: { scale: 0.9 },
         }}
-        initial="folded"
+        initial="folded" // Initial state is folded
         className="relative flex flex-col items-center"
       >
         <motion.div
@@ -51,10 +57,12 @@ const FoldableMap = () => {
           className="grid aspect-video max-h-[80vh] w-full min-w-[600px] p-8"
         >
           <div className="grid grid-cols-3 [grid-area:1/1]">
+            {/* Left section of the map */}
             <motion.div
               style={{ x: xLeftSection, skewY: "-1deg" }}
               className="map-image origin-bottom-right border-r border-[rgba(255,255,255,.1)] shadow-2xl"
             />
+            {/* Center section of the map */}
             <motion.div
               style={
                 {
@@ -64,11 +72,13 @@ const FoldableMap = () => {
               }
               className="map-image brightness-[--brightness]"
             />
+            {/* Right section of the map */}
             <motion.div
               style={{ x: xRightSection, skewY: "1deg" }}
               className="map-image origin-bottom-left border-l border-[rgba(255,255,255,.1)] shadow-2xl"
             />
           </div>
+          {/* Draggable area */}
           <motion.div
             drag="x"
             _dragX={xDrag}
@@ -82,6 +92,7 @@ const FoldableMap = () => {
             className="relative z-10 cursor-grab [grid-area:1/1] active:cursor-grabbing"
           />
         </motion.div>
+        {/* Text below the map */}
         <motion.div
           variants={{
             folded: {
